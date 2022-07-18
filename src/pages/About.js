80b623434c614c9ff44e33f54b220a4bc64f7f54
Janette Campbell <campbjp@hotmail.com>
2022-07-17 22:47:38 -0400

    chore: added some styling to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,10 +2,18 @@ import NavBar from "../layout/NavBar";
 
 const About = () => {
   return (
-    <div>
+    <>
       <NavBar />
-      <h1 className="page-header">About Us</h1>
-    </div>
+      <div className="page-content">
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+          deserunt atque a asperiores vero rerum ratione, accusantium in
+          necessitatibus numquam praesentium non officiis cumque aliquam, earum
+          neque officia minus nam.
+        </p>
+      </div>
+    </>
   );
 };
 
